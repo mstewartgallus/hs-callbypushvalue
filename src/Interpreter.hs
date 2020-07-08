9c2f82e08d826638a77800b9e1f4ea611de77d85
Steven Stewart-Gallus <sstewartgallus@sstewartgallus.com>
2020-07-08 14:07:04 -0700

    Rework interpreter tagless final style

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -12,54 +12,71 @@ import GlobalMap (GlobalMap)
 import qualified GlobalMap
 import VarMap (VarMap)
 import qualified VarMap
+import Variable
 
 evaluate :: Data a -> a
-evaluate = interpretData VarMap.empty
+evaluate x = case abstractData x VarMap.empty of
+  Value value -> value
 
 newtype Id a = Id a
 
-interpretData :: VarMap Id -> Data a -> a
-interpretData _ (ConstantData k) = interpretConstant k
-interpretData env (VariableData v) = case VarMap.lookup v env of
-  Just (Id x) -> x
+data X tag a where
+  Value :: a -> X Data a
+  Act :: (Stack a -> R) -> X Code a
+
+instance Cps X where
+  letTo t f = Value $ PopStack $ \x -> case f (Value x) of
+    Act k -> k NilStack
+  jump (Act x) (Value f) = Act $ \NilStack -> (x f)
+  global g = case GlobalMap.lookup g globals of
+    Just (G x) -> Act x
+    Nothing -> error "global not found in environment"
+  push (Value h) (Value t) = Value (PushStack h t)
+  constant (IntegerConstant x) = Value x
+
+abstractData :: Cps t => Data a -> VarMap (t Data) -> t Data a
+abstractData (ConstantData k) = \_ -> constant k
+abstractData (VariableData v) = \env -> case VarMap.lookup v env of
+  Just x -> x
   Nothing -> error "variable not found in environment"
-interpretData env (LetToData binder body) = PopStack $ \value ->
-  let env' = VarMap.insert binder (Id value) env
-   in interpret env' body NilStack
-interpretData env (PushData h t) =
-  let h' = interpretData env h
-      t' = interpretData env t
-   in PushStack h' t'
-interpretData _ NilStackData = NilStack
+abstractData (LetToData binder@(Variable t _) body) =
+  let body' = abstract body
+   in \env ->
+        letTo t $ \value ->
+          body' (VarMap.insert binder value env)
+abstractData (PushData h t) =
+  let h' = abstractData h
+      t' = abstractData t
+   in \env -> push (h' env) (t' env)
+abstractData NilStackData = \_ -> nilStack
 
-interpret :: VarMap Id -> Code a -> Stack a -> R
-interpret env (ReturnCode value) (PopStack k) =
-  let value' = interpretData env value
-   in k value'
-interpret env (LetBeCode value binder body) k =
-  let value' = interpretData env value
-      env' = VarMap.insert binder (Id value') env
-   in interpret env' body k
-interpret env (LambdaCode variable body) (PushStack h t) =
-  let env' = VarMap.insert variable (Id h) env
-   in interpret env' body t
-interpret _ (GlobalCode g) k =
-  let Just (X g') = GlobalMap.lookup g globals
-   in g' k
-interpret env (JumpCode x f) NilStack =
-  let stack = interpretData env f
-   in interpret env x stack
+abstract :: Cps t => Code a -> VarMap (t Data) -> t Code a
+abstract (ReturnCode value) =
+  let value' = abstractData value
+   in \env -> returns (value' env)
+abstract (LetBeCode value binder body) =
+  let value' = abstractData value
+      body' = abstract body
+   in \env -> body' (VarMap.insert binder (value' env) env)
+abstract (LambdaCode variable@(Variable t _) body) =
+  let body' = abstract body
+   in \env ->
+        lambda t $ \x -> body' (VarMap.insert variable x env)
+abstract (GlobalCode g) =
+  let g' = global g
+   in \_ -> g'
+abstract (JumpCode x f) =
+  let x' = abstract x
+      f' = abstractData f
+   in \env -> jump (x' env) (f' env)
 
-data X a = X (Stack a -> R)
+data G a = G (Stack a -> R)
 
-globals :: GlobalMap X
+globals :: GlobalMap G
 globals =
   GlobalMap.fromList
-    [ GlobalMap.Entry strictPlus (X strictPlusImpl)
+    [ GlobalMap.Entry strictPlus (G strictPlusImpl)
     ]
 
 strictPlusImpl :: Stack (Integer -> Integer -> F Integer) -> R
 strictPlusImpl (PushStack x (PushStack y (PopStack k))) = k (x + y)
-
-interpretConstant :: Constant a -> a
-interpretConstant (IntegerConstant x) = x
