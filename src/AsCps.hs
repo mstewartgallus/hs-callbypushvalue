e4fe63d1148657516d9bff39d662e46a6ab58bae
Steven Stewart-Gallus <sstewartgallus@sstewartgallus.com>
2020-07-23 12:09:02 -0700

    Cleanup cps

--- src/AsCps.hs
+++ src/AsCps.hs
@@ -20,65 +20,67 @@ import HasThunk
 import HasTuple
 
 toContinuationPassingStyle :: (HasCode t, Cps.Cps t) => Code (AsCps t) a -> Data t ('U a)
-toContinuationPassingStyle (CodeCallcc t x) = HasThunk.thunk t x
+toContinuationPassingStyle (C t x) = HasThunk.thunk t x
 
 data AsCps t
 
 instance HasCode t => HasCode (AsCps t) where
-  data Code (AsCps t) a = CodeCallcc (SAlgebra a) (Stack t a -> Code t 'Void)
+  data Code (AsCps t) a = C (SAlgebra a) (Stack t a -> Code t 'Void)
 
 instance HasData t => HasData (AsCps t) where
-  data Data (AsCps t) a = DataCallcc (SSet a) (Data t a)
+  data Data (AsCps t) a = D (SSet a) (Data t a)
 
 instance (HasStack t) => HasStack (AsCps t) where
-  data Stack (AsCps t) a = SB (SAlgebra a) (Stack t a)
+  data Stack (AsCps t) a = S (SAlgebra a) (Stack t a)
 
 instance (HasData t, HasConstants t) => HasConstants (AsCps t) where
-  constant k = DataCallcc (Constant.typeOf k) $ constant k
+  unit = D SUnit unit
+  constant k = D (Constant.typeOf k) $ constant k
 
 instance (HasCode t, Cps.Cps t) => HasReturn (AsCps t) where
-  returns (DataCallcc t x) = CodeCallcc (SF t) $ \k -> Cps.throw k x
+  returns (D t x) = C (SF t) $ \k -> Cps.throw k x
 
 instance (HasLet t) => HasLet (AsCps t) where
-  letBe (DataCallcc t x) f =
-    let CodeCallcc b _ = f (DataCallcc t x)
-     in CodeCallcc b $ \k ->
+  letBe (D t x) f =
+    let C b _ = f (D t x)
+     in C b $ \k ->
           letBe x $ \val ->
-            case f (DataCallcc t val) of
-              CodeCallcc _ f' -> f' k
+            case f (D t val) of
+              C _ f' -> f' k
 
 instance (Cps.Cps t) => HasLetTo (AsCps t) where
-  letTo (CodeCallcc (SF t) x) f =
-    let CodeCallcc b _ = f (DataCallcc t undefined)
-     in CodeCallcc b $ \k ->
+  letTo (C (SF t) x) f =
+    let C b _ = f (D t undefined)
+     in C b $ \k ->
           x
             ( Cps.letTo t $ \val ->
-                case f (DataCallcc t val) of
-                  CodeCallcc _ f' -> f' k
+                case f (D t val) of
+                  C _ f' -> f' k
             )
-  apply (CodeCallcc (_ `SFn` b) f) (DataCallcc _ x) = CodeCallcc b $ \k -> f (Cps.apply x k)
+  apply (C (_ `SFn` b) f) (D _ x) = C b $ \k -> f (Cps.apply x k)
 
-instance (HasCode t, Cps.Cps t) => HasTuple (AsCps t)
+instance (HasCode t, Cps.Cps t) => HasTuple (AsCps t) where
+  pair (D tx x) (D ty y) = D (SPair tx ty) (pair x y)
 
 instance (HasThunk t, Cps.Cps t) => HasThunk.HasThunk (AsCps t) where
-  lambda s@(SB (xt `SFn` r) lam) f =
-    let CodeCallcc ct _ = f (DataCallcc xt undefined) (SB r undefined)
-     in CodeCallcc ct $ \k -> HasThunk.lambda lam $ \x t ->
-          let CodeCallcc _ y = f (DataCallcc xt x) (SB r t)
+  lambda s@(S (xt `SFn` r) lam) f =
+    let C ct _ = f (D xt undefined) (S r undefined)
+     in C ct $ \k -> HasThunk.lambda lam $ \x t ->
+          let C _ y = f (D xt x) (S r t)
            in y k
 
-  thunk t f = DataCallcc (SU t) $ HasThunk.thunk t $ \k ->
-    case f (SB t k) of
-      CodeCallcc _ y -> y Cps.nil
+  thunk t f = D (SU t) $ HasThunk.thunk t $ \k ->
+    case f (S t k) of
+      C _ y -> y Cps.nil
 
-  force (DataCallcc _ th) (SB _ stack) = CodeCallcc SVoid $ \_ ->
+  force (D _ th) (S _ stack) = C SVoid $ \_ ->
     HasThunk.force th stack
 
-  call g (SB _ k) = CodeCallcc SVoid $ \_ -> HasThunk.call g k
+  call g (S _ k) = C SVoid $ \_ -> HasThunk.call g k
 
 instance Cps.Cps t => Callcc.Callcc (AsCps t) where
-  catch t f = CodeCallcc t $ \k -> letLabel k $ \k' ->
-    case f (SB t k') of
-      CodeCallcc _ y -> y Cps.nil
+  catch t f = C t $ \k -> letLabel k $ \k' ->
+    case f (S t k') of
+      C _ y -> y Cps.nil
 
-  throw (SB _ x) (CodeCallcc _ f) = CodeCallcc SVoid $ \_ -> f x
+  throw (S _ x) (C _ f) = C SVoid $ \_ -> f x
