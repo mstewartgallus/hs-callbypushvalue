d1372c761e0d4c00a5163d8841a22d63e72ecd3c
Steven Stewart-Gallus <sstewartgallus@sstewartgallus.com>
2020-07-31 11:08:36 -0700

    Prototype natural transformation module

---
+++ src/NatTrans.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeOperators #-}
+
+module NatTrans ((:~>) (..)) where
+
+import Control.Category
+import Prelude hiding ((.), id)
+
+newtype f :~> g = NatTrans (forall x. f x -> g x)
+
+apply :: f :~> g -> f a -> g a
+apply (NatTrans f) = f
+
+instance Category ((:~>)) where
+  id = NatTrans id
+  (NatTrans f) . (NatTrans g) = NatTrans (f . g)
+
+newtype (:.:) f g x = Compose (f (g x))
+
+(#) :: (Functor k) => f :~> g -> j :~> k -> (j :.: f) :~> (k :.: g)
+NatTrans f # NatTrans g = NatTrans (\(Compose x) -> Compose (fmap f (g x)))
